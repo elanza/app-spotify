9d555d24f6a4bcaf1f99d8d30074e81a176cc8fc
Ernesto Lanza <ernestlanza@gmail.com>
2017-06-16 09:34:18 -0400

    Added bootstrap 4, added app.route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//App route
+import { APP_ROUTING } from './app.route';
+
+//App Components
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeCarouselComponent } from './components/shared/home-carousel/home-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    HomeComponent,
+    NavbarComponent,
+    HomeCarouselComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
