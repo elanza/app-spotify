5e438c6b0a78fe9e589c5a0264737a34d869d020
Ernesto Lanza <ernestlanza@gmail.com>
2017-06-16 09:29:33 -0400

    Added components, homeCarousel, homeComponent, navbarComponent, searchComponent

---
+++ src/app/components/shared/home-carousel/home-carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-carousel',
+  templateUrl: './home-carousel.component.html'
+})
+export class HomeCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
